5ce9c0232c27f140fa926de0f0a72f3c75490d2a
David4888 <74598998+David4888@users.noreply.github.com>
2023-03-15 20:20:49 +0100

    Add files via upload

--- PrimerJs/miScript.js
+++ PrimerJs/miScript.js
@@ -5,7 +5,7 @@ let tarjeta="";
 tarjeta+= ' <div class="card" id="caribe">';
 tarjeta+='<img src="https://images.unsplash.com/photo-1503756234508-e32369269deb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8bWFyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="">';
 tarjeta+='<h2>Caribe</h2>';
-tarjeta+='<p class="fechas">30 marzo</p>';
+tarjeta+='<p class="fechas">15 marzo</p>';
 tarjeta+='<p class="texto">Disfruta de las playas más exóticas en la costa caribeña</p>';
 tarjeta+='<a href="caribe.html">Ver más</a>';
 tarjeta+='</div> ';
